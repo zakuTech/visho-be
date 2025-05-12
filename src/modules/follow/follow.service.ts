eb86793ba711377ec38c61f497aeab35c3288eec
fadliasyp <riansyahfadli12@gmail.com>
2025-05-12 16:51:44 +0700

    menambah try catch

--- src/modules/follow/follow.service.ts
+++ src/modules/follow/follow.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, HttpException, Inject } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 import { PrismaService } from '../prisma/prisma.service';
 import {
@@ -21,18 +20,23 @@ export class FollowService {
   async createFollow(req: FollowCreateRequest): Promise<FollowCreateResponse> {
     this.logger.info(`CREATE FOLLOW WITH USER ID ${req.user_id || '-'}`);
 
-    const result = await this.prisma.followers.create({
-      data: {
-        follower_id: uuidv4(),
-        user_id: req.user_id,
-        follower_user_id: req.follower_user_id,
-      },
-    });
+    try {
+      const result = await this.prisma.followers.create({
+        data: {
+          follower_id: uuidv4(),
+          user_id: req.user_id,
+          follower_user_id: req.follower_user_id,
+        },
+      });
 
-    return {
-      message: 'Success: Follow created',
-      follower_id: result.follower_id,
-    };
+      return {
+        message: 'Success: Follow created',
+        follower_id: result.follower_id,
+      };
+    } catch (error) {
+      this.logger.error(`Failed follow post: ${error.message}`, error.stack);
+      throw new HttpException(error.message, error.status || 500);
+    }
   }
 
   async deleteFollow(
@@ -42,23 +46,31 @@ export class FollowService {
       `DELETE FOLLOW WITH USER ID ${params.follower_user_id || '-'}`,
     );
 
-    const data = await this.prisma.followers.findFirst({
-      where: {
-        follower_id: params.follower_id,
-        follower_user_id: params.follower_user_id,
-      },
-    });
+    try {
+      const data = await this.prisma.followers.findFirst({
+        where: {
+          follower_id: params.follower_id,
+          follower_user_id: params.follower_user_id,
+        },
+      });
 
-    if (!data) {
-      throw new HttpException('Follow not found', 404);
+      if (!data) {
+        throw new HttpException('Follow not found', 404);
+      }
+
+      await this.prisma.followers.delete({
+        where: {
+          follower_id: data.follower_id,
+        },
+      });
+
+      return { message: 'Success: Follow deleted' };
+    } catch (error) {
+      this.logger.error(
+        `Failed to delete follow: ${error.message}`,
+        error.stack,
+      );
+      throw new HttpException('Failed to delete follow', error.status || 500);
     }
-
-    await this.prisma.followers.delete({
-      where: {
-        follower_id: data.follower_id,
-      },
-    });
-
-    return { message: 'Success: Follow deleted' };
   }
 }
