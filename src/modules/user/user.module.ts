60d4d89a8f4c9ce9253b0911cc16ea49d189ae79
fadliasyp <riansyahfadli12@gmail.com>
2025-06-13 23:43:50 +0700

    add photo_profile and cover

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { AuthModule } from '../auth/auth.module';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), SupabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,12 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Inject } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 import { PrismaService } from '../prisma/prisma.service';
-import { UserResponse } from './user.contract';
+import { UploadPhotoAndBioRequest, UserResponse } from './user.contract';
+import { SupabaseService } from '../supabase/supabase.service';
+import { sanitizeFileName } from 'src/media/media.controller';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 interface RegisterRequest {
   username: string;
@@ -13,8 +17,12 @@ interface RegisterRequest {
 @Injectable()
 export class UserService {
   private prisma: PrismaService;
+  @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger;
 
-  constructor(prisma: PrismaService) {
+  constructor(
+    prisma: PrismaService,
+    private readonly supabaseService: SupabaseService,
+  ) {
     this.prisma = prisma;
   }
 
@@ -64,5 +72,116 @@ export class UserService {
       throw new BadRequestException('User not found');
     }
     return user;
+  }
+
+  async uploadPhotoAndBio(
+    req: UploadPhotoAndBioRequest,
+    files: { profile?: Express.Multer.File[]; cover?: Express.Multer.File[] },
+  ): Promise<UserResponse> {
+    this.logger.info(`Update photo or bio request: ${JSON.stringify(req)}`);
+
+    const user = await this.prisma.users.findUnique({
+      where: { user_id: req.user_id },
+    });
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    let photoProfileUrl = user.photo_profile;
+    let coverProfileUrl = user.cover_profile;
+    let photoProfilePath = user.photo_profile_path;
+    let coverProfilePath = user.cover_profile_path;
+
+    if (files?.profile?.[0]) {
+      const file = files.profile[0];
+      const sanitizedFileName = sanitizeFileName(file.originalname);
+      const filename = `${Date.now()}-${sanitizedFileName}`;
+      const profilePath = `test/profiles/${filename}`;
+
+      // Hapus file lama jika ada
+      if (user.photo_profile_path) {
+        await this.supabaseService.deleteFile(
+          process.env.SUPABASE_BUCKET_NAME,
+          user.photo_profile_path,
+        );
+      }
+
+      if (user.photo_profile) {
+        await this.supabaseService.deleteFile(
+          process.env.SUPABASE_BUCKET_NAME,
+          user.photo_profile,
+        );
+      }
+
+      await this.supabaseService.uploadFile(
+        process.env.SUPABASE_BUCKET_NAME,
+        profilePath,
+        file.buffer,
+        file.mimetype,
+      );
+
+      photoProfileUrl = await this.supabaseService.getPublicUrl(
+        process.env.SUPABASE_BUCKET_NAME,
+        profilePath,
+      );
+
+      photoProfilePath = profilePath;
+    }
+
+    if (files?.cover?.[0]) {
+      const file = files.cover[0];
+      const sanitizedFileName = sanitizeFileName(file.originalname);
+      const filename = `${Date.now()}-${sanitizedFileName}`;
+      const coverPath = `test/cover/${filename}`;
+
+      // Hapus file lama jika ada
+      if (user.cover_profile_path) {
+        await this.supabaseService.deleteFile(
+          process.env.SUPABASE_BUCKET_NAME,
+          user.cover_profile_path,
+        );
+      }
+
+      if (user.cover_profile) {
+        await this.supabaseService.deleteFile(
+          process.env.SUPABASE_BUCKET_NAME,
+          user.cover_profile,
+        );
+      }
+
+      await this.supabaseService.uploadFile(
+        process.env.SUPABASE_BUCKET_NAME,
+        coverPath,
+        file.buffer,
+        file.mimetype,
+      );
+
+      coverProfileUrl = await this.supabaseService.getPublicUrl(
+        process.env.SUPABASE_BUCKET_NAME,
+        coverPath,
+      );
+
+      coverProfilePath = coverPath;
+    }
+
+    const updatedUser = await this.prisma.users.update({
+      where: {
+        user_id: user.user_id,
+      },
+      data: {
+        bio: req.bio ?? user.bio,
+        photo_profile: photoProfileUrl,
+        photo_profile_path: photoProfilePath,
+        cover_profile: coverProfileUrl,
+        cover_profile_path: coverProfilePath,
+      },
+    });
+
+    this.logger.info(
+      `Profile atau bio berhasil diperbarui: ${updatedUser.user_id}`,
+    );
+
+    return updatedUser;
   }
 }
